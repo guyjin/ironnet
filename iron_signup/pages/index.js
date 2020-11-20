a04ceca7f564ce678ccc13367fb179f944e24eb7
Ben Vaughan <benjamin.vaughan@gmail.com>
2020-11-20 01:07:20 +0000

    initial commit, establishing the thing runs.

---
+++ iron_signup/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import styles from '../styles/index.module.scss'
+
+function Index () {
+  return (
+    <div className={styles.example}>
+      Hello, Iron Net.
+    </div>
+  )
+}
+
+export default Index
