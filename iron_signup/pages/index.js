0f05e8ac0d150e6e5d6b58bbdf16a9b9a7749f92
bvaughan <benjamin.Vaughan@us.idemia.com>
2020-11-19 21:27:41 -0700

    Finished layout.  Need to get behavior working right.

--- iron_signup/pages/index.js
+++ iron_signup/pages/index.js
@@ -6,7 +6,7 @@ function Index() {
   return (
     <div>
       <Header headerText="Sign In" instructionText="" />
-      <form name="signInForm" id="signInForm" className={styles.forms}>
+      <form name="signInForm" id="signInForm" className="forms">
         <input type="email" name="eMail" id="email" placeholder="Email" />
         <input
           type="password"
@@ -15,7 +15,7 @@ function Index() {
           placeholder="Password"
         />
         <input type="submit" value="Sign In" />
-        <div className={`smallText ${styles.helpText}`}>
+        <div className="smallText helpText">
           Forgot your password?{" "}
           <Link href="reset">
             <a>Reset it here.</a>
--- iron_signup/pages/reset.js
+++ iron_signup/pages/reset.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Header from "../components/header";
 function Reset() {
   return (
@@ -6,9 +7,21 @@ function Reset() {
         headerText="Reset Password"
         instructionText="Enter your registered email in order to receive password reset instructions"
       />
-      <form name="resetByEmail" id="resetByEmail">
-        <input type="emailAddr" name="emailAddr" id="email" />
+      <form name="resetByEmail" id="resetByEmail" className="forms">
+        <input
+          type="email"
+          name="emailAddr"
+          id="emailAddr"
+          placeholder="Email"
+          required
+        />
         <input type="submit" value="Reset Password" />
+        <div className="smallText helpText">
+          Didn't mean to click that?&nbsp;
+          <Link href="/">
+            <a>Sign in.</a>
+          </Link>
+        </div>
       </form>
     </div>
   );
